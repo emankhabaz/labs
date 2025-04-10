d378fb0c0f6832027686d5a93a603e40a41711b0
Georges <georges.younes@gmail.com>
2025-04-11 02:40:23 +0300

    add support files for 04-restful-interfaces

---
+++ assignments/04-restful-interfaces/base/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Decks",
+  description: "",
+};
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
