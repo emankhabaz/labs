fa30d089917261aaecac68922373b52aa56e38bf
emankhabaz <116437085+emankhabaz@users.noreply.github.com>
2025-04-14 11:31:35 +0300

    lab12

---
+++ 12-client-rendering/my-app/app/page.jsx
@@ -0,0 +1,6 @@
+// import Task from "../components/task";
+import Task from "@/components/task";
+
+export default function Home(){
+    return <Task title="Fancy title!" checked={true}></Task>;
+}
---
+++ 12-client-rendering/my-app/components/task.jsx
@@ -0,0 +1,11 @@
+export default function Task({}){
+    return(
+        <div>
+            <p></p>
+            <button>
+
+            </button>
+            <button></button>
+        </div>
+    )
+}
