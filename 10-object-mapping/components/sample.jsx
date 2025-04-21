283abf9ff8a286cb3f18d0e61d4deba6fe551aa2
emankhabaz <116437085+emankhabaz@users.noreply.github.com>
2025-04-21 19:08:10 +0300

    lab10

---
+++ 10-object-mapping/components/sample.jsx
@@ -0,0 +1,7 @@
+import {useState, useEffect} from 'react';
+
+export default function Sample({users}){
+    const [loaded, setLoaded] = useState(0);
+    const [_users, setUsers] = useState(users);
+}
+
